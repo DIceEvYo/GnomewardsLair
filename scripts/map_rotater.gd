041397d4eb940c092094a457b16c40d11f540307
ScriptlessWeek <155108629+ScriptlessWeek@users.noreply.github.com>
2025-05-29 18:40:04 -0400

    added levels to map_container and have it loaded on startup map are offset automatically, so long as they are 32x32 changed filtering to nearest

--- scripts/map_rotater.gd
+++ scripts/map_rotater.gd
@@ -1,9 +1,23 @@
 extends Node2D
 ## Note map rotater should be positioned in the middle of the screen and
-## child map should be offset to center (subtract half of screen size)
+## child map should be offset to center (handled in _ready)
 
+
+var current_level: TileMapLayer
+var levels: Array[PackedScene] = [
+	preload("res://scenes/level_1.tscn"),
+]
 # used to prevent spam rotation
 var ignore_input := false
+
+
+func _ready() -> void:
+	current_level = levels[0].instantiate()
+	add_child(current_level)
+	# 32x32 tilemap with 16x16 tiles = 512x512.
+	# all maps should be 32x32 to remain centered/on screen.
+	current_level.position = Vector2(-256, -256)
+
 
 func _input(event: InputEvent) -> void:
 	if ignore_input:
