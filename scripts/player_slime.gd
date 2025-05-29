50dde43ebe5c41def8e6f48bc736fe4d2de85b8d
ScriptlessWeek <155108629+ScriptlessWeek@users.noreply.github.com>
2025-05-29 19:36:13 -0400

    fixed issue with player slide friction and implemented bounce for player

--- scripts/player_slime.gd
+++ scripts/player_slime.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
 
+var bounce_strength: float = 0.5
+var friction: float = 20
+# used to help calculate bounce
+var previous_y: float = 0
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		previous_y = velocity.y
+	elif previous_y > 100:
+		velocity.y = -previous_y * bounce_strength
+		print(velocity.y)
+		previous_y = velocity.y
 	
-	velocity.move_toward(Vector2.ZERO, delta)
+	velocity = velocity.move_toward(Vector2.ZERO, delta * friction)
 	
 	move_and_slide()
