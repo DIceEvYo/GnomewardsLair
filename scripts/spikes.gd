13d76a1fa6a6e9ca0ac10cbd0f01c04716c04d51
ScriptlessWeek <155108629+ScriptlessWeek@users.noreply.github.com>
2025-06-01 00:53:51 -0400

    added lose sound and lose screen, triggered in spike script atm

--- scripts/spikes.gd
+++ scripts/spikes.gd
@@ -3,4 +3,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerSlime:
+		$LoseSound.play()
+		get_node("/root/Game/lose_screen").visible = true
+		await $LoseSound.finished
 		get_tree().call_deferred("reload_current_scene")
